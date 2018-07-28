666fa7b3ac8c5e2498bac74984dde698c70d8b69
p1nkrabb1t <pinkcyberbunny@gmail.com>
2018-07-28 13:04:08 +0100

    display date in correct format

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import java.util.ArrayList;
 
@@ -58,8 +60,12 @@ public final class QueryUtils {
                 String magnitude = properties.getString("mag");
                 String location = properties.getString("place");
                 String time = properties.getString("time");
+                long timeInMilliseconds = 1454124312220L;
+                Date dateObject = new Date(timeInMilliseconds);
+                SimpleDateFormat dateFormatter = new SimpleDateFormat("MMM DD, yyyy");
+                String dateToDisplay = dateFormatter.format(dateObject);
 
-                Quake earthquake = new Quake(magnitude,location,time);
+                Quake earthquake = new Quake(magnitude,location,dateToDisplay);
                 earthquakes.add(earthquake);
             }
 
